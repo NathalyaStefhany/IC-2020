d9089a49c739b54220a83ba8fe4e1afb69273322
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2020-10-21 00:17:38 -0300

    Adding Unit Test (Camera and AddScore)

--- Arkanoid/Assets/Editor/TestAddScore.cs
+++ Arkanoid/Assets/Editor/TestAddScore.cs
@@ -10,7 +10,7 @@ namespace Tests
     public class TestAddScore
     {
         private AddScore addScore;
-        private HighScoreEntry highScoreEntry1, highScoreEntry2, highScoreEntry3;
+        private HighScoreEntry highScoreEntry1, highScoreEntry2, highScoreEntry3, highScoreEntry4;
         private HighScores highScores;
 
         [SetUp]
@@ -19,8 +19,9 @@ namespace Tests
             addScore = GameObject.FindObjectOfType<AddScore>();
 
             highScoreEntry1 = new HighScoreEntry { name = "NATH", round = 1, score = 1500 };
-            highScoreEntry2 = new HighScoreEntry { name = "STEF", round = 1, score = 3000 };
-            highScoreEntry3 = new HighScoreEntry { name = "PER", round = 1, score = 1000 };
+            highScoreEntry2 = new HighScoreEntry { name = "JOAO", round = 2, score = 3000 };
+            highScoreEntry3 = new HighScoreEntry { name = "ANDR", round = 1, score = 1000 };
+            highScoreEntry4 = new HighScoreEntry { name = "ANDR", round = 1, score = 1500 };
 
             highScores = new HighScores();
 
@@ -39,6 +40,22 @@ namespace Tests
             Assert.AreSame(highScoreEntry2, highScores.highScoreEntryList[0]);
             Assert.AreSame(highScoreEntry1, highScores.highScoreEntryList[1]);
             Assert.AreSame(highScoreEntry3, highScores.highScoreEntryList[2]);
+        }
+
+        [Test]
+        public void testSortByNameWhenScoreEqual()
+        {
+            highScores.highScoreEntryList.Add(highScoreEntry1);
+            highScores.highScoreEntryList.Add(highScoreEntry2);
+            highScores.highScoreEntryList.Add(highScoreEntry3);
+            highScores.highScoreEntryList.Add(highScoreEntry4);
+
+            highScores = addScore.sortScore(highScores);
+
+            Assert.AreSame(highScoreEntry2, highScores.highScoreEntryList[0]);
+            Assert.AreSame(highScoreEntry4, highScores.highScoreEntryList[1]);
+            Assert.AreSame(highScoreEntry1, highScores.highScoreEntryList[2]);
+            Assert.AreSame(highScoreEntry3, highScores.highScoreEntryList[3]);
         }
     }
 }
---
+++ Arkanoid/Assets/PlayModeTests/TestCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestCamera
+    {
+        [UnityTest]
+        public IEnumerator TestCameraExists()
+        {
+            SceneManager.LoadScene("Menu");
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.IsTrue(GameObject.Find("Main Camera"), "Camera não encontrada!");
+        }
+    }
+}
